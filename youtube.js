c09c5120a6fafddccd2457b9f6a2894c93b2a61a
Orlando Mitrut <orlando.mitrut@gmail.com>
2022-10-05 03:21:00 +0300

    Refined youtube script for normal site usage

--- youtube.js
+++ youtube.js
@@ -2,11 +2,21 @@
 // @name     Youtube hide shorts
 // @version  1
 // @grant    none
-// @match    https://www.youtube.com/feed/subscriptions
+// @match    https://www.youtube.com/
 // ==/UserScript==
 
-setTimeout(() => {
-    document.querySelectorAll("ytd-grid-video-renderer").forEach(video => {
-        video.querySelector("a[href*='/shorts/']") ? video.remove() : null;
-    });
-}, 5000);
+function waitAndPurgeShorts(delay){
+    setTimeout(() => {
+        document.querySelectorAll("ytd-grid-video-renderer").forEach(video => {
+            video.querySelector("a[href*='/shorts/']") ? video.remove() : null;
+        });
+    }, delay);
+}
+
+let intervalRef = setInterval(() => {
+    let subs = document.querySelector("a#endpoint[href='/feed/subscriptions']");
+    if (subs) {
+        subs.addEventListener("click", () => waitAndPurgeShorts(3000));
+        clearInterval(intervalRef);
+    }
+}, 500);
