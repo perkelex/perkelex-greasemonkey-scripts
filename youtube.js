273caa9d21ecce420db9c5353bedb5967c4ba538
Orlando Mitrut <orlando.mitrut@gmail.com>
2022-10-05 03:36:27 +0300

    Some more refinement on youtube hide shorts script

--- youtube.js
+++ youtube.js
@@ -2,7 +2,8 @@
 // @name     Youtube hide shorts
 // @version  1
 // @grant    none
-// @match    https://www.youtube.com/
+// @include  https://www.youtube.com/
+// @include  https://www.youtube.com/feed/subscriptions
 // ==/UserScript==
 
 function waitAndPurgeShorts(delay){
@@ -13,10 +14,15 @@ function waitAndPurgeShorts(delay){
     }, delay);
 }
 
-let intervalRef = setInterval(() => {
-    let subs = document.querySelector("a#endpoint[href='/feed/subscriptions']");
-    if (subs) {
-        subs.addEventListener("click", () => waitAndPurgeShorts(3000));
-        clearInterval(intervalRef);
-    }
-}, 500);
+if (location.href.includes("/feed/subscriptions")){
+    waitAndPurgeShorts(3000);
+}
+else{
+    let intervalRef = setInterval(() => {
+        let subs = document.querySelector("a#endpoint[href='/feed/subscriptions']");
+        if (subs) {
+            subs.addEventListener("click", () => waitAndPurgeShorts(3000));
+            clearInterval(intervalRef);
+        }
+    }, 500);
+}
